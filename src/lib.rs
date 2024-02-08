a701ed3c03efed56947922a786730671ee4a01b9
ArnoDarkrose <149183464+ArnoDarkrose@users.noreply.github.com>
2024-02-08 16:37:38 +0300

    implemeted main tech traits

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,46 @@
 //! #Maths is the crate for solving varios maths problems
 mod primitives;
-
-use crate::primitives::checked::*;
+pub use crate::primitives::checked;
 ///This the main mod that primarily contains traits that the whole crate depends on
 mod tech {
-    use std::ops::{Add, Sub, Div, Mul, Neg, AddAssign, SubAssign, DivAssign, MulAssign};
+    //Notice that the names of the imported traits are changed
+    use std::ops::{Add as AddTrait, Sub as SubTrait, Div as DivTrait, Mul as MulTrait, Neg};
+
+    ///Indicates that the struct that implements it can be considered as a representaion of an operation
+    pub trait Op {}
+
+    pub struct Mul {}
+    impl Op for Mul {}
+
+    pub struct Add {}
+    impl Op for Add {}
+
+    pub trait Com<T: Op> {}
+    pub trait Ass<T: Op> {}
+
+    pub trait Group<T: Op> where Self: Ass<T>{
+        fn neut() -> Self;
+    }
+
+    pub trait Abelian<T: Op> where Self: Com<T> {}
+
+    pub trait Ring where Self: 
+        Group<Add> + Abelian<Add> + AddTrait<Self, Output = Self> + MulTrait<Self, Output = Self> +
+        SubTrait<Self, Output = Self> + Neg<Output = Self> + Sized {}
+
+    pub trait UnRing where Self: Ring {
+        fn one() -> Self;
+    }
+
+    pub trait IntegralDomain where Self: Ring + Com<Mul> + Ass<Mul> {} 
+
+    pub trait Field where Self: UnRing + IntegralDomain + DivTrait<Self, Output = Self> {
+        fn zero() -> Self {
+            <Self as Group<Add>>::neut()
+        }
+    }
+
+    //TODO Meta, Gcd ?
 }
 
 //TODO when rewriting Polynomial make it contain array so that i can make some funcs const and for some other benefits
